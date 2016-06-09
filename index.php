d48bbe781d15a3af0a6120ac1c85a3a9e2572f25
sajibhasan <sajib001@gmail.com>
2016-06-09 15:38:46 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo "Hello World";
+
+echo "Today is the second class";
+
+print "Ok I am printing";
+
+echo "<br>";
+
+echo  "and a ", 1, 2, 3;   // comma-separated without parentheses
+echo ("and a 123");        // just one parameter with parentheses
+
+echo "<br>";
+
+print ("and a 123");
+print  "and a 123";
+
+echo "<br>";
+
+$b ? print "true" : print "false";
