3a9587228488037c9129021a048002dd656ea42c
sajibhasan <sajib001@gmail.com>
2016-06-10 22:44:26 +0600

    3rd class comment section

---
+++ comment.php
@@ -0,0 +1,78 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//echo "Mizan";
+/*
+ 
+ this is another comment
+ 
+  
+ */
+
+# testing
+
+
+
+$a = true;
+$a = TRUE;
+
+echo $a;
+
+$b = FALSE;
+$b = false;
+
+echo $b;
+
+$a = -1;
+$b = 0;
+
+if($a)
+{
+    echo "I am true";
+} else {
+    echo "I am false";
+}
+/*
+
+0 1 2 3 4 5 6 7
+
+0 1 2 3 4 5 6 7 8 9 A B C D E F
+
+16| 31
+    ----
+       1      F
+       0      1
+
+10 = 0b1010
+10 = 012
+31 = 0x1F
+        
+0xFF = F X 16  ^ 1 + F X 16 ^ 0
+         = 15 X 16 + 15 X 1
+        = 240 + 15
+        = 255
+        
+        10101 = 1 X 2 ^ 4 + 1 X 2 ^ 2 + 1 X 2 ^ 0
+                    = 16 + 4 +1
+                    = 21
+        
+        755
+        7 = 111
+        5 = 101
+        5 = 101 
+        
+        100 = read
+        010 = write
+        001 = execute
+        
+        
+  */
+
+
+
+
