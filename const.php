6f864b03eb25cbf37033cf7ad817dd99a04b9dec
sajibhasan <sajib001@gmail.com>
2016-06-10 22:57:08 +0600

    constructor section

---
+++ const.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define("____NAME____","TECHMASTERS");
+
+echo ____NAME____;
+
+echo __DIR__;
+
+define("NAME","Mizan");
