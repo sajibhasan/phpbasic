4d76dc3ef37be1b15d7bc74dacd521209c53b9bc
sajibhasan <sajib001@gmail.com>
2016-06-19 02:09:38 +0600

    Reverse Number

---
+++ reverse.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$val = [9,8,7,6,5,4,3,2,1];
+$len = count($val);
+for($i = $len-1; $i >= 0; $i--){
+    echo $val[$i]." , ";
+}
+
+
+
