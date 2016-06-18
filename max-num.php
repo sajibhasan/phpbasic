79a484e8f4379515beca40bd0370c6da655f4255
sajibhasan <sajib001@gmail.com>
2016-06-19 00:39:03 +0600

    Count Maximum Number

---
+++ max-num.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$a = 50;
+$b = 40;
+$c = 20;
+
+if($a > $b && $a > $c){
+    echo $a. " is a Maximum Number";
+}  elseif($b > $c ){
+    
+        echo $b. " is a Maximum Number";
+    
+}else{
+        echo $c." is a Maximum Number";
+    }
