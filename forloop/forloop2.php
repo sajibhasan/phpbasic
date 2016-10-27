9fbfe2877dd2d5fe6414503e3919ff0fd47a02c5
sajibhasan <sajib001@gmail.com>
2016-10-27 11:37:08 +0600

    triangle 1

---
+++ forloop/forloop2.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+for($i=1; $i<=10; $i++){
+    for($j=1; $j<=$i; $j++){
+        echo " *";
+    }
+    echo "<br>";
+}
